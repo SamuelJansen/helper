5c3696dab52dfc6cbe8f3ec31c402383e68a6542
Samuel Jansen <samuel.jansen@cwi.com.br>
2020-07-07 23:41:59 -0300

    feat: package deploy prep

---
+++ helper/api/src/domain/Constant.py
@@ -0,0 +1,50 @@
+WRAPPER =   '[WRAPPER] '
+DEBUG =     '[DEBUG  ] '
+ERROR =     '[ERROR  ] '
+WARNING =   '[WARNING] '
+SUCCESS =   '[SUCCESS] '
+FAILURE =   '[FAILURE] '
+SETTING =   '[SETTING] '
+
+NEW_LINE = '''\n'''
+BAR_N = '''\\n'''
+SPACE = ''' '''
+NOTHING = ''''''
+TRIPLE_SINGLE_QUOTE = """'''"""
+TRIPLE_DOUBLE_QUOTE = '''"""'''
+SINGLE_QUOTE = """'"""
+DOUBLE_QUOTE = '''"'''
+COMA = ''','''
+COLON = ''':'''
+BACK_SLASH = '''\\'''
+SLASH = '''/'''
+HASH_TAG = '''#'''
+SPACE = ''' '''
+DOT = '''.'''
+DASH = '''-'''
+SPACE_DASH_SPACE = ''' - '''
+UNDERSCORE = '''_'''
+DOT_SPACE = '''. '''
+COLON_SPACE = ''': '''
+SPACE_COLON_SPACE = ''' : '''
+LOG_CAUSE = '''Cause'''
+
+TAB_UNITS = 4
+TAB = TAB_UNITS * SPACE
+
+TRUE = 'True'
+FALSE = 'False'
+
+BOOLEAN = 'bool'
+STRING = 'str'
+INTEGER = 'int'
+TUPLE = 'tuple'
+LIST = 'list'
+DICT = 'dict'
+
+OPEN_TUPLE = '('
+CLOSE_TUPLE = ')'
+OPEN_LIST = '['
+CLOSE_LIST = ']'
+OPEN_DICTIONARY = '{'
+CLOSE_DICTIONARY = '}'
---
+++ helper/api/src/service/LogHelper.py
@@ -0,0 +1,37 @@
+from helper.api.src.domain import Constant as c
+
+def softLog(cls,message,level) :
+    if not cls or cls == c.NOTHING :
+        classPortion = c.NOTHING
+    else :
+        classPortion = f'{cls.__name__}{c.COLON_SPACE}'
+    print(f'{level}{classPortion}{message}')
+
+def hardLog(cls,message,exception,level) :
+    if not cls or cls == c.NOTHING :
+        classPortion = c.NOTHING
+    else :
+        classPortion = f'{cls.__name__}{c.COLON_SPACE}'
+    if not exception or exception == c.NOTHING :
+        errorPortion = c.NOTHING
+    else :
+        errorPortion = f'{c.DOT_SPACE}{c.LOG_CAUSE}{c.COLON_SPACE}{str(exception)}'
+    print(f'{level}{classPortion}{message}{errorPortion}')
+
+def success(cls,message) :
+    softLog(cls,message,c.SUCCESS)
+
+def setting(cls,message) :
+    softLog(cls,message,c.SETTING)
+
+def debug(cls,message) :
+    softLog(cls,message,c.DEBUG)
+
+def warning(cls,message) :
+    softLog(cls,message,c.WARNING)
+
+def failure(cls,message,exception) :
+    hardLog(cls,message,exception,c.FAILURE)
+
+def error(cls,message,exception) :
+    hardLog(cls,message,exception,c.ERROR)
